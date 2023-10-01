a85990af30ac934e53eadd8336684933116930c2
Alex Anson <1alex.anson@gmail.com>
2023-10-01 13:28:13 -0400

    defining variables with type annotations

--- ch2/src/main.rs
+++ ch2/src/main.rs
@@ -1,3 +1,23 @@
+fn adding_ints() {
+    let a = 10;
+    let b: i32 = 20;
+    let c = 30i32;
+    let d = 30_i32;
+    let e = add(add(a, b), add(c, d));
+
+    println!("a + b + c + d = {}", e);
+    // ^ macro (!) -- returns code rather than values ...
+    /*
+    - when printing to the console, every input data type has its own way of
+    being represented as a text string.
+    - `println!()` figures out what methods to call on the arguments.
+    */
+}
+
+fn add(i: i32, j: i32) -> i32 {
+    i + j
+}
+
 fn main() {
-    println!("Hello, world!");
+    adding_ints();
 }
